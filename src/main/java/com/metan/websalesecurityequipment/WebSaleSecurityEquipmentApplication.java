7ea53f625264a3de01e2bdfc3d4160a2f63544ed
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2022-04-11 16:47:57 +0700

    init project

---
+++ src/main/java/com/metan/websalesecurityequipment/WebSaleSecurityEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.metan.websalesecurityequipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSaleSecurityEquipmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSaleSecurityEquipmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metan/websalesecurityequipment/WebSaleSecurityEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metan.websalesecurityequipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSaleSecurityEquipmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
