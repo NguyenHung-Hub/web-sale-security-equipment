5744d878414a34a1e8c1d0da6cf5350204fcc45f
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2022-05-05 16:10:16 +0700

    home

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        const header = document.getElementById("header");
-        const sticky = header.offsetTop;
-
-        if (window.pageYOffset > sticky) {
-            header.classList.add("sticky");
-        } else {
-            header.classList.remove("sticky");
-        }
-    });
+    // $(window).scroll(function () {
+    //     const header = document.getElementById("header");
+    //     const sticky = header.offsetTop;
+    //
+    //     if (window.pageYOffset > sticky) {
+    //         header.classList.add("sticky");
+    //     } else {
+    //         header.classList.remove("sticky");
+    //     }
+    // });
 
     $('#search-input-mobile').focus(function () {
         $(".header-mobile .nav-menu-mobile .search-form-mobile").addClass("scroll-top")
-        $(".over-suggest").css({"opacity": "1", "visibility": "visible"})
+        $(".over-suggest").css({"display": "block"})
     });
 
     $(".over-suggest").click(function () {
-        $(this).css({"opacity": "0", "visibility": "hidden"})
+        $(this).css({"display": "none"})
         $(".header-mobile .nav-menu-mobile .search-form-mobile").removeClass("scroll-top")
     });
 
