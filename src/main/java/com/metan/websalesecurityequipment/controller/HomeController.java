4c9f03189d13ea1214ffe5b3c5bc651ea0f4476e
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2022-05-06 14:26:27 +0700

    fix build dependency

--- src/main/java/com/metan/websalesecurityequipment/controller/HomeController.java
+++ src/main/java/com/metan/websalesecurityequipment/controller/HomeController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
-@RequestMapping
 public class HomeController {
 
     private final ProductService productService;
@@ -27,19 +25,6 @@ public class HomeController {
     }
 
     @GetMapping
-    public String showFormHome(Model model) {
-        List<Product> products = productService.findAll();
-        List<Product> topProducts = productService.findTopProduct();
-        List<Category> categories = categoryService.findAll();
-
-        model.addAttribute("PRODUCT_LIST", products);
-        model.addAttribute("TOP_PRODUCT_LIST", topProducts);
-        model.addAttribute("CATEGORIES", categories);
-
-        return "home";
-    }
-
-    @GetMapping("/home-demo")
     public String showFormHomeDemo(Model model) {
         List<Product> products = productService.findAll();
         List<Product> topProducts = productService.findTopProduct();
@@ -49,7 +34,7 @@ public class HomeController {
         model.addAttribute("TOP_PRODUCT_LIST", topProducts);
         model.addAttribute("CATEGORIES", categories);
 
-        return "home-demo";
+        return "home";
     }
 
 }
