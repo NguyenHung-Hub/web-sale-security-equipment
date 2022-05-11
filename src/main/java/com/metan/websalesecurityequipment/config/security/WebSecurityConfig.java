da74718aebc69ed1e292ae9532be5c6c05f20b4d
Hung <daminhnguyenhung@gmail.com>
2022-05-11 09:51:55 +0700

    add product

--- src/main/java/com/metan/websalesecurityequipment/config/security/WebSecurityConfig.java
+++ src/main/java/com/metan/websalesecurityequipment/config/security/WebSecurityConfig.java
@@ -76,7 +76,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-//                .antMatchers("/").permitAll()
+                .antMatchers("/**").permitAll()
                 .antMatchers("/account/**").permitAll()
                 .antMatchers("/oauth2/**").permitAll()
                 .anyRequest().authenticated()
